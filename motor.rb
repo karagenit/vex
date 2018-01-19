709ed1cd8d5141f0b4a9fe154d7895abd1a4e49b
Caleb Smith <karagenit@outlook.com>
2018-01-18 20:18:15 -0500

    Add Quantity Argument

--- motor.rb
+++ motor.rb
@@ -3,6 +3,11 @@ class Motor
   attr_reader :stall_torque
   attr_reader :free_speed
   attr_reader :max_power
+  attr_reader :quantity
+
+  def initialize
+    @quantity = 1
+  end
 
   def current(speed: nil, torque: nil)
     unless speed.nil?
@@ -33,13 +38,28 @@ class Motor
       return @stall_torque * current / @stall_current
     end
   end
+
+  ##
+  # Sets the # of motors being used.
+  #
+  # Note: because this may be called in initialize, we rely on
+  # the @quantity value to default to one (and not be nil) - this
+  # is set in the super initialize in Motor.
+  #
+  def quantity=(qty)
+    @stall_current *= qty / @quantity
+    @stall_torque *= qty / @quantity
+    @max_power *= qty / @quantity
+  end
 end
 
 class CIM < Motor
-  def initialize
+  def initialize(qty = 1)
+    super()
     @stall_current = 131
     @stall_torque = 2.41
     @free_speed = 5330
     @max_power = 337
+    quantity = qty
   end
 end
